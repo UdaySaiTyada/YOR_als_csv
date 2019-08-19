c1d4b603010246d5c738db4ed0cae4a258fcf55b
Uday Sai Tyada <udaysai2551998@gmail.com>
2019-08-19 15:13:28 +0530

    Code Cleaned

--- YOR_DataScience_Applications/urls.py
+++ YOR_DataScience_Applications/urls.py
@@ -18,11 +18,6 @@ from django.urls import path
 from AffordableLiving import views
 urlpatterns = [
     path('', views.affordableLiving),
-    # path('admin/', admin.site.urls),
-    path('affliving/',views.getRecommendedProperties),
-    # path('module2/', views.module2),
-    # path('module3/', views.module3),
-    # path('report/',views.report),
-    path('getRecommendedProperties/',views.central_model)
+    path('getRecommendedProperties/',views.getRecommendedProperties)
 
 ]
